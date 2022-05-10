1e72d9a1222c8e5bbad7929fb58faba359dc25b5
Fahadmohammad1 <205fahadmohammad@gmail.com>
2022-05-10 23:12:27 +0600

    background image set

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,10 +1,20 @@
 import React from "react";
 import Banner from "./Banner";
+import bg from "../../assets/images/bg.png";
 
 const Home = () => {
   return (
     <div>
-      <Banner />
+      <div
+        style={{
+          backgroundImage: `url(${bg})`,
+          backgroundRepeat: "no-repeat",
+          backgroundPositionY: "center",
+        }}
+        className="bg-cover"
+      >
+        <Banner />
+      </div>
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,9 +7,9 @@ module.exports = {
     themes: [
       {
         doctortheme: {
-          primary: "#0FCFEC",
-          secondary: "#19D3AE",
-          accent: "#3A4256",
+          primary: "rgba(15, 207, 236, 1)",
+          secondary: "rgba(25, 211, 174, 1)",
+          accent: "rgba(48, 56, 76, 1)",
           neutral: "#3d4451",
           "base-100": "#ffffff",
         },
