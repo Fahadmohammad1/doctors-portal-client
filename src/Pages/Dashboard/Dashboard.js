7e4ddf381ae7a66b3822512ef4fb17cf9e3a9b95
Fahadmohammad1 <205fahadmohammad@gmail.com>
2022-05-19 18:53:21 +0600

    add doctor route added

--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -27,9 +27,14 @@ const Dashboard = () => {
             <Link to="/dashboard/review">My Reviews</Link>
           </li>
           {admin && (
-            <li>
-              <Link to="/dashboard/users">All Users</Link>
-            </li>
+            <>
+              <li>
+                <Link to="/dashboard/users">All Users</Link>
+              </li>
+              <li>
+                <Link to="/dashboard/addDoctor">Add a Doctor</Link>
+              </li>
+            </>
           )}
         </ul>
       </div>
